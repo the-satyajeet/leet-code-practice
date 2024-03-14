5b7dc3c1dc0f2329c670b85a0adc220b9c8f5529
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-03-14 23:44:46 +0530

    added

--- 605 Can Place Flowers.cpp
+++ 605 Can Place Flowers.cpp
@@ -16,3 +16,26 @@ public:
         return false;
     }
 };
+
+
+/*
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int s = flowerbed.size();
+        int count = 0;
+        for(int i = 0; i < s; ++i) {
+            if(flowerbed[i] == 0) {
+                int prev = (i == 0) ? 0 : flowerbed[i-1];
+                int next = (i == s - 1) ? 0 : flowerbed[i+1];
+
+                if(prev == 0 && next == 0) {
+                    flowerbed[i] = 1;
+                    count += 1;
+                }
+            }
+        }
+        return count >= n;
+    }
+};
+*/
